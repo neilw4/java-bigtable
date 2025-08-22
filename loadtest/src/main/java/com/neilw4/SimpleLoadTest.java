aa389dda318be6a61739f5c0ff7ebf4364ac0fa4
Neil Wells <neilwells@google.com>
2025-08-22 20:30:20 +0000

    hacks for testing purposes

--- loadtest/src/main/java/com/neilw4/SimpleLoadTest.java
+++ loadtest/src/main/java/com/neilw4/SimpleLoadTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.bigtable;
+package com.neilw4;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
